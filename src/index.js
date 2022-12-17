4afd80b9a1b290d0cac58b587a0267bbb4af76b2
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-12-17 17:16:05 -0300

    add: nginx dockerfile

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const mysql = require("mysql");
+
+// const dbConnection = {
+//   host: "db",
+//   user: "root",
+//   password: "root",
+//   database: "nodedb",
+//   port: 3306,
+// };
+
+// const connectionInstance = mysql.createConnection(dbConnection);
+
+let dataNames = [];
+
+// connectionInstance.connect((error) => {
+//   if (error) throw error;
+
+//   console.log("connected to mysql database!");
+
+//   connectionInstance.query("CREATE DATABASE IF NOT EXISTS nodedb;");
+
+//   connectionInstance.query(
+//     "CREATE TABLE IF NOT EXISTS people(id int not null auto_increment, name varchar(255), PRIMARY KEY (id));"
+//   );
+
+//   connectionInstance.query(
+//     "INSERT INTO people(name) VALUES ('Mikkaiser Camisa 10');"
+//   );
+
+//   connectionInstance.query("SELECT * from people;", (error, result, fields) => {
+//     if (error) throw error;
+
+//     dataNames = result;
+//   });
+// });
+
+http
+  .createServer((request, response) => {
+    response.write("<h1>Full Cycle Rocks!</h1>");
+
+    dataNames.map((item) => {
+      response.write(`<h2>${item.name}</h2>`);
+    });
+
+    response.end();
+  })
+  .listen(8080);
